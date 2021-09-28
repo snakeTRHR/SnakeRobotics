19c47c1356c095ef3ea62a4e207a9560e4aebf23
TsuruharaKota <m1830@s.akashi.ac.jp>
2021-09-29 07:35:15 +0900

    success

--- frenet_serret.cpp
+++ frenet_serret.cpp
@@ -7,6 +7,8 @@
 #include "/usr/include/eigen3/Eigen/Sparse"
 #include "/usr/include/eigen3/Eigen/Core"
 #include "/usr/include/eigen3/Eigen/LU"
+#include "matplotlibcpp.h"
+namespace plt = matplotlibcpp;
 
 double a = 1;
 double b = 0.1;
@@ -29,17 +31,17 @@ Eigen::Matrix<double, 3, 1> Func_3(Eigen::Matrix<double, 3, 1> input_1){
 
 int main(){
     //(e_r, e_p, e_s)
-    Eigen::Matrix<double, 3, 1> c_s;
+    Eigen::Matrix<double, 3, 1> C;
     Eigen::Matrix<double, 3, 1> T;
     Eigen::Matrix<double, 3, 1> N;
     Eigen::Matrix<double, 3, 1> B;
     //set initial value
-    c_s << 0, 0, 0;
+    C << 0, 0, 0;
     T << 1, 0, 0;
     N << 0, 1, 0;
     B << 0, 0, 1;
 
-    int n = 10;
+    int n = 1000;
     std::vector<double> val_x(n), val_y(n), val_z(n);
 
     //RungeKutta
@@ -61,6 +63,9 @@ int main(){
     Eigen::Matrix<double, 3, 1> K_d_3;
 
     double t = 0;
+
+    std::vector<double> T_x, T_y, T_z;
+
     for(int s = 0; s < n; ++s){
         K_a_1 = h * Func_1(N);
         K_a_2 = h * Func_2(T, B);
@@ -84,7 +89,20 @@ int main(){
         T += (K_a_1 + 2 * K_b_1 + 2 * K_c_1 + K_d_1) / 6;
         N += (K_a_2 + 2 * K_b_2 + 2 * K_c_2 + K_d_2) / 6;
         B += (K_a_3 + 2 * K_b_3 + 2 * K_c_3 + K_d_3) / 6;
-
+        C += t * h * T;
         std::cout << T << std::endl;
+
+        T_x.push_back(C(0, 0));
+        T_y.push_back(C(1, 0));
+        T_z.push_back(C(2, 0));
     }
+    std::map<std::string, std::string> keywords;
+    keywords.insert(std::pair<std::string, std::string>("label", "parametric curve") );
+
+    plt::plot3(T_x, T_y, T_z, keywords);
+    plt::xlabel("x label");
+    plt::ylabel("y label");
+    plt::set_zlabel("z label"); // set_zlabel rather than just zlabel, in accordance with the Axes3D method
+    plt::legend();
+    plt::show();
 }
