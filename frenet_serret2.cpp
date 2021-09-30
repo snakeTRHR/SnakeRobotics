6c14e1d7374fb1e004949408051dd3e9473f8de4
TsuruharaKota <m1830@s.akashi.ac.jp>
2021-09-30 11:40:15 +0900

    fix

--- frenet_serret2.cpp
+++ frenet_serret2.cpp
@@ -10,26 +10,31 @@
 #include "matplotlibcpp.h"
 namespace plt = matplotlibcpp;
 
-double a = 1;
-double b = 0.1;
+double a = 2;
+double b = 4;
 
 double curvature(double _s){
     //return a / (a * a + b * b);
-    return 1 / 0.5;
     //return std::abs(sin(_s * 0.5)) / std::pow(cos(_s) *cos(_s) + 1, 3.0/2.0);
+    return 5.0 / 4.0;
 }
 double torsion(double _s){
     //return b / (a * a + b * b);
-    return 0;
+    //return 1.0 / 4.0;
+    return 0.0;
+    //return 0;
 }
 Eigen::Matrix<double, 3, 1> Func_1(double _s, Eigen::Matrix<double, 3, 1> input_1){
     return (curvature(_s) * input_1);
 }
 Eigen::Matrix<double, 3, 1> Func_2(double _s, Eigen::Matrix<double, 3, 1> input_1, Eigen::Matrix<double, 3, 1> input_2){
-    return (-1 * curvature(_s) * input_1 + torsion(_s) * input_2);
+    return ((-1 * curvature(_s) * input_1) + (torsion(_s) * input_2));
 }
 Eigen::Matrix<double, 3, 1> Func_3(double _s, Eigen::Matrix<double, 3, 1> input_1){
     return (-1 * torsion(_s) * input_1);
+}
+Eigen::Matrix<double, 3, 1> Func_4(double _s, Eigen::Matrix<double, 3, 1> input_1){
+    return (input_1);
 }
 
 int main(){
@@ -52,66 +57,73 @@ int main(){
     Eigen::Matrix<double, 3, 1> K_a_1;
     Eigen::Matrix<double, 3, 1> K_a_2;
     Eigen::Matrix<double, 3, 1> K_a_3;
+    Eigen::Matrix<double, 3, 1> K_a_4;
 
     Eigen::Matrix<double, 3, 1> K_b_1;
     Eigen::Matrix<double, 3, 1> K_b_2;
     Eigen::Matrix<double, 3, 1> K_b_3;
+    Eigen::Matrix<double, 3, 1> K_b_4;
 
     Eigen::Matrix<double, 3, 1> K_c_1;
     Eigen::Matrix<double, 3, 1> K_c_2;
     Eigen::Matrix<double, 3, 1> K_c_3;
+    Eigen::Matrix<double, 3, 1> K_c_4;
 
     Eigen::Matrix<double, 3, 1> K_d_1;
     Eigen::Matrix<double, 3, 1> K_d_2;
     Eigen::Matrix<double, 3, 1> K_d_3;
+    Eigen::Matrix<double, 3, 1> K_d_4;
 
     double s = 0;
 
-    std::vector<double> T_x, T_y, T_z;
+    std::vector<double> C_x, C_y, C_z;
 
     for(int i = 0; i < n; ++i){
-        s = h * i;
         K_a_1 = h * Func_1(s, N);
         K_a_2 = h * Func_2(s, T, B);
         K_a_3 = h * Func_3(s, N);
+        K_a_4 = h * Func_4(s, T);
 
         K_b_1 = h * Func_1(s + h / 2, N + K_a_2 / 2);
         K_b_2 = h * Func_2(s + h / 2, T + K_a_1 / 2, B + K_a_3 / 2);
         K_b_3 = h * Func_3(s + h / 2, N + K_a_2 / 2);
+        K_b_4 = h * Func_4(s + h / 2, T + K_a_1 / 2);
 
 
         K_c_1 = h * Func_1(s + h / 2, N + K_b_2 / 2);
         K_c_2 = h * Func_2(s + h / 2, T + K_b_1 / 2, B + K_b_3 / 2);
         K_c_3 = h * Func_3(s + h / 2, N + K_b_2 / 2);
+        K_c_4 = h * Func_4(s + h / 2, T + K_b_1 / 2);
 
         K_d_1 = h * Func_1(s + h, N + K_c_2);
         K_d_2 = h * Func_2(s + h, T + K_c_1, B + K_c_3);
         K_d_3 = h * Func_3(s + h, N + K_c_2);
+        K_d_4 = h * Func_4(s + h, T + K_c_1);
 
-        //s += h;
+        s += h;
 
         T += (K_a_1 + 2 * K_b_1 + 2 * K_c_1 + K_d_1) / 6;
         N += (K_a_2 + 2 * K_b_2 + 2 * K_c_2 + K_d_2) / 6;
         B += (K_a_3 + 2 * K_b_3 + 2 * K_c_3 + K_d_3) / 6;
-        C += T;
-        std::cout << C << std::endl;
+        C += (K_a_4 + 2 * K_b_4 + 2 * K_c_4 + K_d_4) / 6;
 
-        T_x.push_back(C(0, 0));
-        T_y.push_back(C(1, 0));
-        T_z.push_back(C(2, 0));
+        C_x.push_back(C(0, 0));
+        C_y.push_back(C(1, 0));
+        C_z.push_back(C(2, 0));
     }
+
     std::cout << "s " << s << std::endl;
     std::map<std::string, std::string> keywords;
     keywords.insert(std::pair<std::string, std::string>("label", "parametric curve") );
 
-    //plt::plot3(T_x, T_y, T_z, keywords);
-    plt::plot(T_x, T_y);
+    plt::plot3(C_x, C_y, C_z, keywords);
+    //plt::plot(C_x, C_z);
     plt::xlabel("x label");
     plt::ylabel("y label");
 
-    //plt::xlim(-15, 15);
-    //plt::ylim(-5, 25);
-    //plt::set_zlabel("z label"); // set_zlabel rather than just zlabel, in accordance with the Axes3D method
+    //plt::xlim(-1, 1);
+    //plt::ylim(0, 2);
+    plt::set_zlabel("z label"); // set_zlabel rather than just zlabel, in accordance with the Axes3D method
     //plt::legend();
     plt::show();
 }
