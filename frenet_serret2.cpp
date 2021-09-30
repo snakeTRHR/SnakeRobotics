f1caf7d60f57669615dcf72248578d0df7b751a6
TsuruharaKota <m1830@s.akashi.ac.jp>
2021-09-30 22:57:02 +0900

    fix

--- frenet_serret2.cpp
+++ frenet_serret2.cpp
@@ -10,16 +10,16 @@
 #include "matplotlibcpp.h"
 namespace plt = matplotlibcpp;
 
-double r = 1.0;
-double h = 0.5;
+double r_c = 1.0;
+double h_c = 0.5;
 
 double curvature(double _s){
-    return r / (r * r + h * h);
+    return r_c / (r_c * r_c + h_c * h_c);
     //return std::abs(sin(_s * 0.5)) / std::pow(cos(_s) *cos(_s) + 1, 3.0/2.0);
-    //return 5.0 / 4.0;
+    //return 1 / 0.5;
 }
 double torsion(double _s){
-    return h / (r * r + h * h);
+    return h_c / (r_c * r_c + h_c * h_c);
     //return 1.0 / 4.0;
     //return 0.0;
     //return 0;
@@ -49,11 +49,7 @@ int main(){
     N << 0, 1, 0;
     B << 0, 0, 1;
 
-    int n = 1000;
-    std::vector<double> val_x(n), val_y(n), val_z(n);
-
     //4次ルンゲクッタ
-    double h = 0.05;
     Eigen::Matrix<double, 3, 1> K_a_1;
     Eigen::Matrix<double, 3, 1> K_a_2;
     Eigen::Matrix<double, 3, 1> K_a_3;
@@ -74,8 +70,10 @@ int main(){
     Eigen::Matrix<double, 3, 1> K_d_3;
     Eigen::Matrix<double, 3, 1> K_d_4;
 
+    double s_long = 100;
     double s = 0;
-
+    double h = 0.05;
+    double n = s_long / h;
     std::vector<double> C_x, C_y, C_z;
     std::vector<double> T_x, T_y, T_z;
 
@@ -106,35 +104,28 @@ int main(){
         T += (K_a_1 + 2 * K_b_1 + 2 * K_c_1 + K_d_1) / 6;
         N += (K_a_2 + 2 * K_b_2 + 2 * K_c_2 + K_d_2) / 6;
         B += (K_a_3 + 2 * K_b_3 + 2 * K_c_3 + K_d_3) / 6;
-        //C += (K_a_4 + 2 * K_b_4 + 2 * K_c_4 + K_d_4) / 6;
+        C += (K_a_4 + 2 * K_b_4 + 2 * K_c_4 + K_d_4) / 6;
 
-        T_x.push_back(T(0, 0));
-        T_y.push_back(T(1, 0));
-        T_z.push_back(T(2, 0));
+        C_x.push_back(C(0, 0));
+        C_y.push_back(C(1, 0));
+        C_z.push_back(C(2, 0));
     }
-
+/*
     //数値積分を行う
-    //シンプソンの積分公式を用いる(区間[a,b]をn分割)
-    for(int i = 0; i < n; ++i){   
-        std::cout << "i = " << i << std::endl; 
-        Eigen::Matrix<double, 3, 1> value;
-        double start = 0, finish = (i * h) - h;
-        double separate_num = 100;
-        double width = (finish - start) / separate_num;
-        double x = 0, y = 0, z = 0;
-        for(int k = 0; k < separate_num; ++k){
-            x = T_x[i] * width;
-            y = T_x[i] * width;
-            z = T_x[i] * width;
-        }
+    double x = 0, y = 0, z = 0;
+    for(int k = 0; k < n; ++k){
+        x += T_x[k] * h;
+        y += T_y[k] * h;
+        z += T_z[k] * h;
         C_x.push_back(x);
-        C_x.push_back(y);
-        C_x.push_back(z);
+        C_y.push_back(y);
+        C_z.push_back(z);
     }
-
+*/
+    //matplotlibで表示
     std::map<std::string, std::string> keywords;
     keywords.insert(std::pair<std::string, std::string>("label", "parametric curve") );
-    //plt::plot3(C_x, C_y, C_z, keywords);
+    plt::plot3(C_x, C_y, C_z, keywords);
     //plt::plot(C_x, C_z);
     plt::xlabel("x label");
     plt::ylabel("y label");
