f12971e6d0cd3f5f82138652afe0e870a03fe708
TsuruharaKota <m1830@s.akashi.ac.jp>
2021-10-02 18:24:55 +0900

    fix

--- frenet_serret.py
+++ frenet_serret.py
@@ -5,35 +5,10 @@ from mpl_toolkits.mplot3d import Axes3D
 
 r_c = 1.0
 h_c = 0.5
-"""
-def curvature(_s):
-    return r_c / (r_c * r_c + h_c * h_c)
-
-def torsion(_s):
-    return h_c / (r_c * r_c + h_c * h_c)
-"""
 def func_frenet_serret(var , s, curvature, torsion):
-    """
-    A = np.array([[                1,                  0,          0]       
-                  [                0,       curvature(s),          0],
-                  [-1 * curvature(s),                  0, torsion(s)],
-                  [                 0, -1 * curvature(s),          0]])
-    F = np.array([[var[0],  var[1],  var[2]],
-                  [var[3],  var[4],  var[5]],
-                  [var[6],  var[7],  var[8]],
-                  [var[9], var[10], var[11]]])
-    dFds = np.dot(A, F)
-    return [dFds]
-    """
     T = [var[3],  var[4],  var[5]]
     N = [var[6],  var[7],  var[8]]
     B = [var[9], var[10], var[11]]
-    """
-    dCds = T
-    dTds = curvature(s) * N
-    dNds = -1 * curvature(s) * T + torsion(s) * B
-    dBds = -1 * torsion(s) * N
-    """
     dCds = T
     dTds = np.dot(curvature,N)
     dNds = np.dot(-1 * curvature, T) + np.dot(torsion, B)
@@ -56,9 +31,11 @@ def plot3d(t_list, var_list):
 
 
 if (__name__ == '__main__'):
-    s_list = np.linspace(0.0, 100.0, 10000)
-    curvature = 1.0 / 0.5;
-    torsion = 0.0;
+    s_list = np.linspace(0, 30.0, 10000)
+    r = 1.0;
+    h = 0.5;
+    curvature = r / (r * r + h * h);
+    torsion = h / (r * r + h * h);
     c_0 = [0, 0, 0]
     t_0 = [1, 0, 0]
     n_0 = [0, 1, 0]
