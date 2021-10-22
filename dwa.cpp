75b531b980b3b13226063edca7f5d889a4388119
TsuruharaKota <m1830@s.akashi.ac.jp>
2021-10-22 17:43:05 +0900

    fix

--- dwa.cpp
+++ dwa.cpp
@@ -6,17 +6,34 @@
 #include"matplotlibcpp.h"
 
 namespace plt = matplotlibcpp;
-//x, y, th, u_v, u_th
-using path_tuple = std::tuple<std::vector<double>, std::vector<double>, std::vector<double>, double, double>;
-//x, y, size
-using obs_tuple = std::tuple<double, double, double>;
-//x, y
-using info_tuple = std::tuple<double, double, double>;
-//
-using next_tuple = std::tuple<std::vector<double>, std::vector<double>, std::vector<double>>;
-//min_ang_velo, max_ang_velo, min_velo, max_velo
-using range_tuple = std::tuple<double, double, double, double>;
-//myenigma参考
+
+struct PathSet{
+    std::vector<double> x;
+    std::vector<double> y;
+    std::vector<double> th;
+    double u_v;
+    double u_th;
+};
+
+struct NextPathSet{
+    std::vector<double> next_x;
+    std::vector<double> next_y;
+    std::vector<double> next_th;
+};
+
+struct Range{
+    double min_ang_velo;
+    double max_ang_velo;
+    double min_velo;
+    double max_velo;
+};
+
+struct ObsSet{
+    double x;
+    double y;
+    double r;
+};
+
 class DWA{
     public:
         DWA(double _g_x, double _g_y){
@@ -26,7 +43,7 @@ class DWA{
         }
         std::vector<double> plot_x;
         std::vector<double> plot_y;
-        void Animation(double _x, double _y, std::vector<obs_tuple> _obs_pos){
+        void Animation(double _x, double _y, std::vector<ObsSet> _obsPos){
             plot_x.push_back(_x);
             plot_y.push_back(_y);
             plt::clf();
@@ -34,11 +51,11 @@ class DWA{
             plt::ylim(-12, 12);
             plt::plot(plot_x, plot_y);
             
-            std::vector<path_tuple> paths = traj_paths.back();
+            std::vector<PathSet> paths = traj_paths.back();
             for(int i = 0; i < paths.size(); ++i){
-                path_tuple temp_path = paths[i];
-                std::vector<double> temp_path_x = std::get<0>(temp_path);
-                std::vector<double> temp_path_y = std::get<1>(temp_path);
+                PathSet tempPath = paths[i];
+                std::vector<double> temp_path_x = tempPath.x;
+                std::vector<double> temp_path_y = tempPath.y;
                 plt::plot(temp_path_x, temp_path_y, "y");
             }
             //write_area
@@ -53,11 +70,11 @@ class DWA{
             plt::plot(area_write_x, area_write_y, "r");
             
             //write circle
-            for(int i = 0; i < _obs_pos.size(); ++i){
-                obs_tuple temp_obs = _obs_pos[i];
-                double center_x = std::get<0>(temp_obs);
-                double center_y = std::get<1>(temp_obs);
-                double radius = std::get<2>(temp_obs);
+            for(int i = 0; i < _obsPos.size(); ++i){
+                ObsSet tempObs = _obsPos[i];
+                double center_x = tempObs.x;
+                double center_y = tempObs.y;
+                double radius = tempObs.r;
                 std::vector<double> write_x;
                 std::vector<double> write_y;
                 for(int k = 0; k < 100; ++k){
@@ -79,25 +96,25 @@ class DWA{
             plt::plot(goal_write_x, goal_write_y, "r");
            
             //write opt path
-            path_tuple temp_opt = traj_opt.back();
-            std::vector<double> temp_x = std::get<0>(temp_opt);
-            std::vector<double> temp_y = std::get<1>(temp_opt);
+            PathSet tempOpt = traj_opt.back();
+            std::vector<double> temp_x = tempOpt.x;
+            std::vector<double> temp_y = tempOpt.y;
             plt::plot(temp_x, temp_y, "g");
             
             plt::named_plot("DWA", plot_x, plot_y);
             plt::legend();
             plt::pause(0.01);
         }
-        void runToGoal(std::vector<obs_tuple> _obs_pos){
+        void runToGoal(std::vector<ObsSet> _obs_pos){
             bool goal_flag = false;
             double time_step = 0;
             while(goal_flag == false){
                 obs = _obs_pos;
 
-                path_tuple opt_path = calcInput();
+                PathSet optPath = calcInput();
 
-                double u_v = std::get<3>(opt_path);
-                double u_th = std::get<4>(opt_path);
+                double u_v = optPath.u_v;
+                double u_th = optPath.u_th;
                 robotUpdateState(u_th, u_v, sampling_time);
                 double dis_to_goal = std::sqrt(std::pow((g_x - robot_x), 2) + std::pow((g_y - robot_y), 2));
                 if(dis_to_goal < 0.5){
@@ -164,27 +181,27 @@ class DWA{
         //サンプリングタイム
         const double sampling_time = 0.1;
         //重み付け
-        const double weight_angle = 500.0;
+        const double weight_angle = 0.4;
         const double weight_velo = 0.2;
-        const double weight_obs = 0.01;
+        const double weight_obs = 0.1;
 
-        std::vector<std::vector<path_tuple>> traj_paths;
-        std::vector<path_tuple> traj_opt;
+        std::vector<std::vector<PathSet>> traj_paths;
+        std::vector<PathSet> traj_opt;
 
         double g_x;
         double g_y;
 
-        std::vector<obs_tuple> obs;
+        std::vector<ObsSet> obs;
 
-        path_tuple calcInput(){
+        PathSet calcInput(){
             //path作成
-            std::vector<path_tuple> paths = makePath();
+            std::vector<PathSet> paths = makePath();
             //path評価
-            path_tuple opt_path = evalPath(paths);
+            PathSet opt_path = evalPath(paths);
             traj_opt.push_back(opt_path);
             return opt_path;
         }
-        range_tuple calcRangeVelos(){
+        Range calcRangeVelos(){
             //角速度
             double range_ang_velo = sampling_time * max_ang_accelation;
             double min_ang_velo = robot_u_th - range_ang_velo;
@@ -212,9 +229,14 @@ class DWA{
             if(max_velo > lim_max_velo){
                 max_velo = lim_max_velo;
             }
-            return range_tuple(min_ang_velo, max_ang_velo, min_velo, max_velo);
+            Range min_max_range;
+            min_max_range.min_ang_velo = min_ang_velo;
+            min_max_range.max_ang_velo = max_ang_velo;
+            min_max_range.min_velo = min_velo;
+            min_max_range.max_velo = max_velo;
+            return min_max_range;
         }
-        next_tuple predictState(double _ang_velo, double _velo, double _x, double _y, double _th, double _dt, double _pre_step){
+        NextPathSet predictState(double _ang_velo, double _velo, double _x, double _y, double _th, double _dt, double _pre_step){
             //予想状態を作成する
             std::vector<double> next_xs;
             std::vector<double> next_ys;
@@ -235,23 +257,31 @@ class DWA{
                 _y = temp_y;
                 _th = temp_th;
             }
-            return next_tuple(next_xs, next_ys, next_ths);
+            NextPathSet nextSet;
+            nextSet.next_x = next_xs;
+            nextSet.next_y = next_ys;
+            nextSet.next_th = next_ths;
+            return nextSet;
         }
-        std::vector<path_tuple> makePath(){
+        std::vector<PathSet> makePath(){
             //角度と速度の組み合わせを全探索
-            std::vector<path_tuple> paths;
-            range_tuple min_max_val = calcRangeVelos();
-            double min_ang_velo = std::get<0>(min_max_val);
-            double max_ang_velo = std::get<1>(min_max_val);
-            double min_velo = std::get<2>(min_max_val);
-            double max_velo = std::get<3>(min_max_val);
+            std::vector<PathSet> paths;
+            Range minMaxVal = calcRangeVelos();
+            double min_ang_velo = minMaxVal.min_ang_velo;
+            double max_ang_velo = minMaxVal.max_ang_velo;
+            double min_velo = minMaxVal.min_velo;
+            double max_velo = minMaxVal.max_velo;
 
             for(double ang_velo = min_ang_velo; ang_velo < max_ang_velo; ang_velo += delta_ang_velo){
                 for(double velo = min_velo; velo < max_velo; velo += delta_velo){
                     double temp_x, temp_y, temp_th;
-                    next_tuple next_path = predictState(ang_velo, velo, robot_x, robot_y, robot_th, sampling_time, pre_step);
-                    path_tuple path;
-                    path = path_tuple(std::get<0>(next_path), std::get<1>(next_path), std::get<2>(next_path), velo, ang_velo);
+                    NextPathSet nextPath = predictState(ang_velo, velo, robot_x, robot_y, robot_th, sampling_time, pre_step);
+                    PathSet path;
+                    path.x = nextPath.next_x;
+                    path.y = nextPath.next_y;
+                    path.th = nextPath.next_th;
+                    path.u_v = velo;
+                    path.u_th = ang_velo;
                     paths.push_back(path);
                 }
             }
@@ -270,24 +300,17 @@ class DWA{
             }
             return _angle;
         }
-        double headingAngle(path_tuple _path, double _g_x, double _g_y){
-            std::vector<double> temp_x = std::get<0>(_path);
-            std::vector<double> temp_y = std::get<1>(_path);
-            std::vector<double> temp_th = std::get<2>(_path);
+        double headingAngle(PathSet _path, double _g_x, double _g_y){
+            std::vector<double> temp_x = _path.x;
+            std::vector<double> temp_y = _path.y;
+            std::vector<double> temp_th = _path.th;
             double last_x = temp_x.back();
             double last_y = temp_y.back();
             double last_th = temp_th.back();
 
             //角度計算
-            //std::cout << "robot_th " << robot_th * 180 / M_PI<< std::endl;
-            std::cout << "th " << last_th * 180 / M_PI << std::endl;
-            //std::cout << "val:" << _g_y << " " << last_y << " " << _g_x << " " << last_x << std::endl;
             double angle_to_goal = atan2((_g_y - last_y), (_g_x - last_x));
-            //double angle_to_goal = atan2((_g_x - last_x), (_g_y - last_y));
-            std::cout << "goal " << angle_to_goal * 180.0 / M_PI << std::endl;
-            //score計算
             double score_angle = angle_to_goal - last_th;
-            //std::cout << "score " << score_angle << std::endl;
 
             //ぐるぐる防止
             score_angle = abs(angleRangeCorrector(score_angle));
@@ -297,40 +320,39 @@ class DWA{
             std::cout << "score " << score_angle * 180 / M_PI << std::endl;
             return score_angle;
         }
-        double headingVelo(path_tuple _path){
-            double temp_path_u_v = std::get<3>(_path);
+        double headingVelo(PathSet _path){
+            double temp_path_u_v = _path.u_v;
             double score_heading_velo = temp_path_u_v;
-            //std::cout << "heading velo:" << score_heading_velo << std::endl;
             return score_heading_velo;
         }
-        std::vector<obs_tuple> calcNearestObs(std::vector<obs_tuple> _obstacles){
-            std::vector<obs_tuple> nearest_obs;
+        std::vector<ObsSet> calcNearestObs(std::vector<ObsSet> _obstacles){
+            std::vector<ObsSet> nearestObs;
             //double area_dis_to_obs = 5;
             double area_dis_to_obs = 2;
             for(int i = 0; i < _obstacles.size(); ++i){
-                obs_tuple temp_obs = _obstacles[i];
-                double temp_dis_to_obs = std::sqrt(std::pow(robot_x - std::get<0>(temp_obs), 2) + std::pow(robot_y - std::get<1>(temp_obs), 2));
+                ObsSet tempObs = _obstacles[i];
+                double temp_dis_to_obs = std::sqrt(std::pow(robot_x - tempObs.x, 2) + std::pow(robot_y - tempObs.y, 2));
                 if(temp_dis_to_obs < area_dis_to_obs){
-                    nearest_obs.push_back(obs[i]);
+                    nearestObs.push_back(obs[i]);
                 }
             }
-            return nearest_obs;
+            return nearestObs;
         }
-        double obstacleCheck(path_tuple _path, std::vector<obs_tuple> _nearest_obs){
+        double obstacleCheck(PathSet _path, std::vector<ObsSet> _nearestObs){
             double score_obstacle = 0.0;
             double temp_dis_to_obs = 0.0;
             
-            std::vector<double> temp_path_x = std::get<0>(_path);
-            std::vector<double> temp_path_y = std::get<1>(_path);
+            std::vector<double> temp_path_x = _path.x;
+            std::vector<double> temp_path_y = _path.y;
             for(int i = 0; i < temp_path_x.size(); ++i){
-                for(int k = 0; k < _nearest_obs.size(); ++k){
-                    obs_tuple temp_obs = _nearest_obs[k];
-                    temp_dis_to_obs = std::sqrt(std::pow((temp_path_x[i] - std::get<0>(temp_obs)), 2) + std::pow((temp_path_y[i] - std::get<1>(temp_obs)), 2));
+                for(int k = 0; k < _nearestObs.size(); ++k){
+                    ObsSet temp_obs = _nearestObs[k];
+                    temp_dis_to_obs = std::sqrt(std::pow((temp_path_x[i] - temp_obs.x), 2) + std::pow((temp_path_y[i] - temp_obs.y), 2));
                     
                     if(temp_dis_to_obs < score_obstacle){
                         score_obstacle = temp_dis_to_obs;
                     }
-                    if(temp_dis_to_obs < std::get<2>(temp_obs) + 0.75){
+                    if(temp_dis_to_obs < temp_obs.r + 0.75){
                         score_obstacle = -INFINITY;
                     }
                     std::cout << "999999" << std::endl;
@@ -356,8 +378,8 @@ class DWA{
                 _obs = (_obs - min_data) / (max_data - min_data);
             }
         }
-        path_tuple evalPath(std::vector<path_tuple> _paths){
-            std::vector<obs_tuple> nearest_obs = calcNearestObs(obs);
+        PathSet evalPath(std::vector<PathSet> _paths){
+            std::vector<ObsSet> nearestObs = calcNearestObs(obs);
             
             std::vector<double> score_heading_angles;
             std::vector<double> score_heading_velos;
@@ -366,7 +388,7 @@ class DWA{
             for(int i = 0; i < _paths.size(); ++i){
                 score_heading_angles.push_back(headingAngle(_paths[i], g_x, g_y));
                 score_heading_velos.push_back(headingVelo(_paths[i]));
-                score_obstacles.push_back(obstacleCheck(_paths[i], nearest_obs));
+                score_obstacles.push_back(obstacleCheck(_paths[i], nearestObs));
             }
             //正規化
             for(int i = 0; i < _paths.size(); ++i){
@@ -377,35 +399,24 @@ class DWA{
             double score = 0.0;
             double temp_score = 0.0;
             
-            path_tuple opt_path;
+            PathSet optPath;
             for(int i = 0; i < _paths.size(); ++i){
                 temp_score = weight_angle * score_heading_angles[i] +
                              weight_velo * score_heading_velos[i] +
                              weight_obs * score_obstacles[i];
                 //std::cout << score_obstacles[i] << std::endl;
                 if(temp_score > score){
-                    opt_path = _paths[i];
+                    optPath = _paths[i];
                     score = temp_score;
                 }
             }
             //std::cout << "score " << score << std::endl;
-            return opt_path;
+            return optPath;
         }
 };
 
 int main(){
-    std::vector<obs_tuple> obsPos;
-    obsPos.push_back(obs_tuple(-4, 1, 0.25));
-    obsPos.push_back(obs_tuple(0, 4.5, 0.25));
-    obsPos.push_back(obs_tuple(3, 4.5, 0.25));
-    obsPos.push_back(obs_tuple(5, 3.5, 0.25));
-    obsPos.push_back(obs_tuple(7.5, 9.0, 0.25));
-    obsPos.push_back(obs_tuple(6, 6, 0.25));
-    obsPos.push_back(obs_tuple(7, 9.0, 0.25));
-    obsPos.push_back(obs_tuple(10, 2.0, 0.25));
-    obsPos.push_back(obs_tuple(10, 1.0, 0.25));
-    obsPos.push_back(obs_tuple(10, 0.0, 0.25));
-    obsPos.push_back(obs_tuple(11, -1.0, 0.25));
+    std::vector<ObsSet> obsPos;
     double goal_x = 10;
     double goal_y = 10;
 
